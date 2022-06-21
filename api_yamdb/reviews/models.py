6ca678cf23cf77c0f662738715c6cca99775eb9d
Stanislav <stas.kh@inbox.ru>
2022-06-21 23:32:16 +0300

    Создал заготовки для моделей

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,11 +2,27 @@ from django.db import models
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        unique=True,
+        max_length=50
+    )
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(models.Model):
-    pass
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        unique=True,
+        max_length=50
+    )
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class Title(models.Model):
