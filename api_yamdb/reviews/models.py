954ff9dff0c8c3d1e2ee9c42f2a2048f31f40c0e
Stanislav <stas.kh@inbox.ru>
2022-06-21 22:46:44 +0300

    Создал заготовки для моделей

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Title(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
