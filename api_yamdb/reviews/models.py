a76d06172469d849fdb333ad052a74af28c23d34
Stanislav Kh <stas.kh@inbox.ru>
2022-06-22 09:31:17 +0300

    поправил модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField("Категория", max_length=256)
+    slug = models.SlugField(
+        unique=True,
+        max_length=50
+    )
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(models.Model):
+    name = models.CharField("Жанр", max_length=256)
+    slug = models.SlugField(
+        unique=True,
+        max_length=50
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField("Произведение", max_length=256)
+    # year =
+
+    def __str__(self):
+        return self.name
