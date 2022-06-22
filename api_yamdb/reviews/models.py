a38b3d11c9b38dba2befa6827d1766dab8ea2a33
korecbtc <99734892+korecbtc@users.noreply.github.com>
2022-06-22 16:02:48 +0300

    Update models.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,40 @@
+from django.db import models
+from django.contrib.auth import get_user_model
 
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        unique=True,
+        max_length=50
+    )
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        unique=True,
+        max_length=50
+    )
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField("Произведение", max_length=256)
+    # year =
+
+    def __str__(self):
+        return self.name
 
 
 class Review(models.Model):
