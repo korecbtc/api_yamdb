531ccc69ebc085cacbcc926a3d9b5f117faf9e8f
Stanislav <stas.kh@inbox.ru>
2022-06-26 18:44:57 +0300

    Поправил ошибки

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -25,3 +25,8 @@ class IsModerator(permissions.BasePermission):
                 request.user.is_superuser
             ) or (request.user.role == 'admin')
         )
+
+
+class IsReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
